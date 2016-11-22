c2b684e00ccee1d1b4fa24b87e7e6e8af3ac9d96
Mark Aufdencamp <Mark@Aufdencamp.com>
2016-11-21 19:19:59 -0500

    Routes to merchant controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,27 @@
 CatarsePaymentWepay::Engine.routes.draw do
-  resources :wepay do
-    collection do
-      post :ipn
-    end
+  #resources :wepay do
+    #collection do
+    #  post :ipn
+    #end
 
-    member do
-      post :refund
-      get  :review
-      post :pay
-      get  :success
-    end
+    #member do
+    #  post :refund
+    #  get  :review
+    #  post :pay
+    #  get  :success
+    #end
         
-  end
+  #end
 
-  resources :merchant, path: "merchant" do
+  #resources :merchant, path: "merchant" do
       
-  end
+  #end
   
-  get 'merchant_setup' => 'merchant#setup'
-  post 'merchant_setup/:project_id' => 'merchant#setup'
-  get 'oauth_redirect_uri' => 'merchant#oauth_account_creation'
-  root to: "merchant#index"
+  get 'user_setup/:user_id/:proj_id' => 'merchant#user_setup', as: 'wepay_user_setup'
+  get 'wepay_oauth_redirect/:user_id/:proj_id' => 'merchant#oauth_account_creation', as: 'wepay_oauth_redirect'
+  get 'account_setup/:user_id/:proj_id' => 'merchant#account_setup', as: 'wepay_account_setup'
+
+  get ':user_id/:proj_id' => "merchant#index", as: 'wepay_account_info'
+  get ':user_id' => "merchant#index", as: 'wepay_user_info'
+  get '/' => 'merchant#index', as: 'wepay_root'
 end
