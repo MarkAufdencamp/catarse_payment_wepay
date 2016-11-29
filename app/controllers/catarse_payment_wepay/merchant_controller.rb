9296b6f8687985b624c590829020830113e58a89
Mark Aufdencamp <Mark@Aufdencamp.com>
2016-11-28 20:06:13 -0500

    Refactor of merchant_controller.  Added paginated user list.  Added WePayAccount info.  Fixed oauth_redirect_uri in user_setup. New OAuth account_creation logic and success/fail view.  New account_setup logic/sequence.  Fixed we_pay_account to not use user_id.

--- app/controllers/catarse_payment_wepay/merchant_controller.rb
+++ app/controllers/catarse_payment_wepay/merchant_controller.rb
@@ -8,12 +8,15 @@ module CatarsePaymentWepay
 #    before_action :authenticate_user!
     before_filter :factory_wepay_client
 
+    #before_action :authenticate_user!
+
     def index
       # params contains user_id, project_id from routed url structure
       #puts params
       if params[:user_id] then @user_id = params[:user_id] end
       if params[:proj_id] then @proj_id = params[:proj_id] end
-      puts @user_id
+      #puts @user_id
+      #puts @proj_id
       
       if @user_id
         @we_pay_user = we_pay_user
@@ -32,10 +35,11 @@ module CatarsePaymentWepay
       when !@user_id
         # if Admin
         # TODO: Display paginated WePayUser/WePayToken list with link to WePayUser/WePayAccount list
+        @we_pay_users = CatarsePaymentWepay::WePayUser.paginate(:page => params[:page], :per_page => 25)
         # else !Admin
         # TODO: Display No Access
-        puts 'index'
-        render 'index'
+        #puts 'index'
+        render 'user_list'
         
       when @user_id && !@proj_id
         # if current_user.id == User_id
@@ -43,10 +47,16 @@ module CatarsePaymentWepay
         # TODO: Display WePayToken if exists
         # TODO: Display WePayUser if exists
         # TODO: Display WePayAccount's for User_id
+        puts @access_token, @user_id
+        if @we_pay_user
+          @we_pay_accounts = CatarsePaymentWepay::WePayAccount.where(:user_id => @user_id).paginate(:page => params[:page], :per_page => 10)
+          render 'user_token_account_info'
+        else
+          render 'invalid_user'
+        end
         # else
         # TODO: Display No Access
-        puts 'user_token_account_info'
-        render 'user_token_account_info'
+        #puts 'user_token_account_info'
         
       when @user_id && @proj_id && !@access_token
         # if current_user.id == User_id
@@ -54,7 +64,7 @@ module CatarsePaymentWepay
         # TODO: Display user_setup link - wepay_user_setup_path(:user_id => @user_id, :proj_id => @proj_id)
         # else
         # TODO: Display No Access
-        puts 'oauth_authentication'
+        #puts 'oauth_authentication'
         render 'oauth_authentication'
                 
       when @user_id && @proj_id && @access_token && !@we_pay_account
@@ -65,7 +75,7 @@ module CatarsePaymentWepay
         # TODO: Display WePayAccount creation link for @user_id, @proj_id - wepay_project_setup_path(:user_id => @user_id, :project_id => @proj_id)
         # else
         # TODO: Display No Access
-        puts 'account_creation'
+        #puts 'account_creation'
         render 'account_creation'
 
       when @user_id && @proj_id && @access_token && @we_pay_account
@@ -75,7 +85,7 @@ module CatarsePaymentWepay
         # TODO: Display WePayAccount's for @user_id, @proj_id
         # else
         # TODO: Display No Access
-        puts  'account_info'
+        #puts  'account_info'
         render 'account_info'
         
       end
@@ -86,10 +96,11 @@ module CatarsePaymentWepay
       # wepay_user_setup_path(:user_id => @user_id, :proj_id => @proj_id)
       #puts params
       @user_id = params[:user_id]
-      @proj_id = params[:project_id]
+      @proj_id = params[:proj_id]
       
       # redirect url from route helper method
-      oauth_redirect_uri = wepay_oauth_redirect_url(:user_id => @user_id, :proj_id => @projid)
+      oauth_redirect_uri = wepay_oauth_redirect_url(:user_id => @user_id, :proj_id => @proj_id)
+      #puts oauth_redirect_uri
       oauth_uri =  @wepay.oauth2_authorize_url(oauth_redirect_uri)
       #puts oauth_uri
      
@@ -102,34 +113,48 @@ module CatarsePaymentWepay
       # params contains either code, or error_code from WePay OAuth Website parameters
       #puts params
 
-      if params[:code]
+      if params[:code] && params[:user_id] && params[:proj_id]
         @oauth_code = params[:code]
         @user_id = params[:user_id]
-        @proj_id = params[:project_id]
+        @proj_id = params[:proj_id]
+
+        @we_pay_token = we_pay_token
+        if @we_pay_token
+          @access_token = we_pay_token[:access_token]
+        else
+          create_wepay_token
+        end
+        
+        if @access_token
+          create_wepay_user
+        end
+        render 'oauth_success'
       else
         # TODO: Edge guard that error_code, error_description were returned   
         @error_code = params[:error_code]
         @error_description = params[:eror_description]
+        render 'oauth_failure'
       end
       
-      we_pay_token = CatarsePaymentWepay::WePayToken.where(:user_id => @user_id).first()
-      if we_pay_token
-        @access_token = we_pay_token[:token]
-      else
-        create_wepay_token
-      end
-      
-      if @access_token
-        create_wepay_user
-      end
     end
     
     def account_setup
       @user_id = params[:user_id]
-      @proj_id = params[:project_id]
+      @proj_id = params[:proj_id]
       project_name = params[:name]
       project_desc = params[:desc]
-      create_wepay_account project_name, project_desc
+
+      @we_pay_token = we_pay_token
+      if @we_pay_token
+        @access_token = we_pay_token[:access_token]
+      end
+
+      if @access_token
+        create_wepay_account project_name, project_desc
+        render 'account_creation_success'
+      else
+        render 'account_creation_failure'
+      end
     end
         
   private
@@ -154,9 +179,9 @@ module CatarsePaymentWepay
         @access_token = access_token
         # TODO: Store WePay Token - Test for existance first?
         we_pay_token = CatarsePaymentWepay::WePayToken.create(
-          :userId => @user_id,
-          :wepayUserId => wepay_user_id,
-          :accessToken => access_token)
+          :user_id => @user_id,
+          :wepay_user_id => wepay_user_id,
+          :access_token => access_token)
       else
         # TODO: Edge Guard - Token denial or failure
         we_pay_token = nil
@@ -165,14 +190,23 @@ module CatarsePaymentWepay
 
     def create_wepay_user
       response = @wepay.call('/user', @access_token)
-      #puts response
+      puts response
+
+      wepay_user_id = response['user_id']
+      first_name = response['first_name']
+      last_name = response['last_name']
+      user_name = response['user_name']
+      email = response['email']
+      state = response['state']
+      
       @we_pay_user = CatarsePaymentWepay::WePayUser.create(
-        :userId => @user_id,
-        :wepayUserId => response[:user_id],
-        :firstName => response[:first_name],
-        :lastName => response[:last_name],
-        :email => response[:email],
-        :state => response[:state])        
+        :user_id => @user_id,
+        :wepay_user_id => wepay_user_id,
+        :first_name => first_name,
+        :last_name => last_name,
+        :user_name => user_name,
+        :email => email,
+        :state => state)        
     end
 
     def create_wepay_account(project_name, project_desc)
@@ -180,17 +214,25 @@ module CatarsePaymentWepay
         :name        => project_name,
         :description => project_desc
       })
-      #puts response
+      puts response
+      account_id = response['account_id']
+      name = response['name']
+      state = response['state']
+      desc = response['description']
+      owner_user_id = response['owner_user_id']
+      balances = response['balances']
+      statuses = response['statuses']
+      
       @we_pay_account = CatarsePaymentWepay::WePayAccount.create(
-        :accountId => response[:account_id],
-        :name => response[:name],
-        :state => response[:state],
-        :desc => response[:description],
-        :ownerUserId => response[:owner_user_id],
-        :balances => response[:balances],
-        :statuses => response[:statuses],
-        :userId => @user_id,
-        :projId => @proj_id
+        :account_id => account_id,
+        :name => name,
+        :state => state,
+        :desc => desc,
+        :owner_user_id => owner_user_id,
+        :balances => balances,
+        :statuses => statuses,
+        :user_id => @user_id,
+        :proj_id => @proj_id
       )
     end
     
@@ -203,7 +245,7 @@ module CatarsePaymentWepay
     end
      
     def we_pay_account
-      we_pay_account = CatarsePaymentWepay::WePayAccount.where(:user_id => @user_id, :proj_id => @proj_id)
+      we_pay_account = CatarsePaymentWepay::WePayAccount.find_by_proj_id @proj_id.to_i
     end
      
   end
