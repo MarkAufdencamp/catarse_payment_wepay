de37bee3454085782487ca05d9177668c8eff988
Mark Aufdencamp <Mark@Aufdencamp.com>
2016-11-28 20:07:24 -0500

    Added project_account, project_balances, wepay_name, and wepay_id methods to top level CatarsePaymentWepay module.

--- lib/catarse_payment_wepay.rb
+++ lib/catarse_payment_wepay.rb
@@ -4,4 +4,25 @@ module CatarsePaymentWepay
   module Processors
     
   end
+  
+  def project_account(proj_id)
+    we_pay_account = CatarsePaymentWepay::WePayAccount.find_by_proj_id proj_id
+    we_pay_account.account_id
+  end
+  
+  def project_balances(proj_id)
+    we_pay_account = CatarsePaymentWepay::WePayAccount.find_by_proj_id proj_id
+    we_pay_account.balances
+  end
+  
+  def wepay_name(user_id)
+    we_pay_user = CatarsePaymentWepay::WePayUser.find_by_user_id user_id
+    we_pay_user.user_name
+  end
+
+  def wepay_id(user_id)
+    we_pay_user = CatarsePaymentWepay::WePayUser.find_by_user_id user_id
+    we_pay_user.wepay_user_id
+  end
+  
 end
